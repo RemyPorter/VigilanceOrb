58aa286b30ce7495ecafc55dd114004fecc11185
Remy <t3knomanser@gmail.com>
2026-06-03 17:48:47 -0400

    Midi stub

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from parser import load, parse
 from actions import apply
 from pathlib import Path
 from actions.audio import get_audio
+import mido
 
 def preload(parsed):
     for event in parsed:
@@ -9,6 +10,21 @@ def preload(parsed):
             if "play" in item:
                 item["play"] = Path(item["play"])
                 get_audio().preload(item["play"])
+
+DEVICE_CONTAINS="Mini"
+class Midi:
+    def __init__(self):
+        input_ports = mido.get_input_names()
+        for port in input_ports:
+            if DEVICE_CONTAINS in port:
+                self._input = mido.open_input(port)
+                return
+    
+    def spin(self, script):
+        for msg in self._input:
+            breakpoint()
+
+
 def main():
     script = load(Path("scripts/test.yaml"))
     parsed = parse(script)
